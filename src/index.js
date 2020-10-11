6ac0c8c8e45f4ce7a44da75be6912193802be795
Onildo Aguiar <onildo.aguiar@live.com>
2020-10-11 11:55:43 -0400

    feat: add with domain obfuscated option

--- src/index.js
+++ src/index.js
@@ -2,26 +2,35 @@
 
 const NUMBER_OF_ASTERIKS = 5;
 const NUMBER_OF_CHARACTERS = 2;
+const WITH_DOMAIN_OBFUSCATED = false;
 
 module.exports = (email, options) => {
     let emailObfuscate = '';
-    let numberOfAsteriks = options && options.numberOfAsteriks ? options.numberOfAsteriks : NUMBER_OF_ASTERIKS;
-    let numberOfCharacters = options && options.numberOfCharacters ? options.numberOfCharacters : NUMBER_OF_CHARACTERS;
+    let parts = [];
+    const numberOfAsteriks = options && options.numberOfAsteriks ? options.numberOfAsteriks : NUMBER_OF_ASTERIKS;
+    const numberOfCharacters = options && options.numberOfCharacters ? options.numberOfCharacters : NUMBER_OF_CHARACTERS;
+    const withDomainObfuscate = options && options.withDomainObfuscated ? options.withDomainObfuscated : WITH_DOMAIN_OBFUSCATED;
 
     try {
-        const parts = email.split('@');
+        parts = email.split('@');
+    } catch (err) {
+        throw new Error('Invalid email format ' + email);
+    } finally {
         const endPosition = parts[0].length <= numberOfCharacters ? parts[0].length / 2 : numberOfCharacters;
+        const domainEndPosition = parts[1].length <= numberOfCharacters ? parts[1].length / 2 : numberOfCharacters;
 
         let asteriks = '';
-
+    
         for (let i = 0; i < numberOfAsteriks; i++) {
             asteriks += '*';
         }
-
-        emailObfuscate = `${parts[0].substring(0, endPosition)}${asteriks}@${parts[1]}`;
-
+        
+        if (withDomainObfuscate){
+            emailObfuscate = `${parts[0].substring(0, endPosition)}${asteriks}@${parts[1].substring(0, domainEndPosition)}${asteriks}`;
+        } else {
+            emailObfuscate = `${parts[0].substring(0, endPosition)}${asteriks}@${parts[1]}`;
+        }
+    
         return emailObfuscate;
-    } catch (err) {
-        throw new Error('Invalid email format ' + email);
     }
 }
